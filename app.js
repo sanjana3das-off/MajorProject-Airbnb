162071e56ec93c167f257f1959ac986fd5be716c
sanjana das <sanjana3das@gmail.com>
2025-10-17 12:44:48 +0530

    SERVER-SIDE VALIDATION -added wrapAsync function in the utils folder created ExpressError class for handling errors then in the app.js files added codes of handling errors ,default handle error, then page not found if all the above route is not matched then added custom error handler such as 400 status code if title is not mentioned.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+
 //index route-setting ejs
 const path = require("path"); //next create  a foldername views
 const { log } = require("console");
@@ -20,6 +21,8 @@ app.listen(8080, () => {
   console.log("app is listening on port 8080");
 });
 
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 app.get("/", (req, res) => {
   res.send("home route");
 });
@@ -53,53 +56,89 @@ main()
 // });
 
 //index route
-app.get("/listings", async (req, res) => {
-  let allListing = await Listing.find({}); //
-  res.render("listings/index.ejs", { allListing });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    let allListing = await Listing.find({}); //
+    res.render("listings/index.ejs", { allListing });
+  })
+);
 //new route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
 //show route - printing data of individual listing
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 // create route
-app.post("/listings", async (req, res) => {
-  // let { title, description, image, price, country, location } = req.body;
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for Title");
+    }
+    // let { title, description, image, price, country, location } = req.body;
 
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //Update route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for Title");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //delete route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
 
-  res.redirect(`/listings/`);
+    res.redirect(`/listings/`);
+  })
+);
+
+//sending standard response for * match all the routes above and if it didnot match then send response of page not found
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
 });
 
+//custom middleware for error handling server side
+app.use((err, req, res, next) => {
+  //first wee will destrut from error messgae
+  let { statusCode = 500, message = "Something went Wrong " } = err; //the above error wil be catch here
+
+  res.status(statusCode).send(message); // and it will send the response
+});
 app.get("/debug", async (req, res) => {
   const listings = await Listing.find({});
   res.send(listings);
